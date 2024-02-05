be8ef0ef9a7c431570918acc6c58ad76c99c682f
JvVenna <joaocovinha12@gmail.com>
2024-02-05 12:07:12 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './components/assets/css/App.css'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 function App() {
 
